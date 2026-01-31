d6b18a3d4c8cc47f75713da97233ee50b921d0c0
Guido <guido@nusaf.co.za>
2026-01-31 11:58:43 +0200

    Remove duplicate migration

--- backend/prisma/migrations/20260131120000_add_primary_warehouse_id/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable: Add primary_warehouse column to users table
-ALTER TABLE "users" ADD COLUMN "primary_warehouse" "Warehouse";
-
--- AlterTable: Add primary_warehouse column to companies table
-ALTER TABLE "companies" ADD COLUMN "primary_warehouse" "Warehouse";
