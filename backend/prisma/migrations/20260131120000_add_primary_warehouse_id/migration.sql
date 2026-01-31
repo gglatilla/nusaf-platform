ecbc512b8eeb91d4580ee638699e4c086040d60e
Guido <guido@nusaf.co.za>
2026-01-31 11:31:30 +0200

    Add migration for primary_warehouse columns on users and companies

--- backend/prisma/migrations/20260131120000_add_primary_warehouse_id/migration.sql
+++ backend/prisma/migrations/20260131120000_add_primary_warehouse_id/migration.sql
@@ -1,5 +1,5 @@
--- AlterTable: Add primary_warehouse to users table
+-- AlterTable: Add primary_warehouse column to users table
 ALTER TABLE "users" ADD COLUMN "primary_warehouse" "Warehouse";
 
--- AlterTable: Add primary_warehouse to companies table
+-- AlterTable: Add primary_warehouse column to companies table
 ALTER TABLE "companies" ADD COLUMN "primary_warehouse" "Warehouse";
